41c63a06f0049f830c6fae62f4719a3d8af3caac
MahmoudAlkenzyy <105129954+MahmoudAlkenzyy@users.noreply.github.com>
2024-02-16 16:10:23 +0200

    add validation using yup

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,8 +1,17 @@
 import { useFormik } from 'formik';
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
-import Yup from 'yup';
-
+import * as yup from 'yup';
+const signupSchema = yup.object({
+    name: yup.string().required('name must be req').min(3).max(12),
+    email: yup.string().email(),
+    password: yup.string().required('password must be req').min(6).max(6),
+    rePassword: yup.string().required('must be req').min(6).max(6),
+    phone: yup
+        .string()
+        .required('phone must be req')
+        .matches(/^01[125][0-9]{8}/),
+});
 function SignUp() {
     const userData = {
         name: '',
@@ -12,13 +21,15 @@ function SignUp() {
         phone: '',
     };
     function submitHandler(values) {
-        console.log('submiting...0, ', values);
+        // console.log('submiting...0, ', values);
     }
 
-    const { handleSubmit, handleChange, values } = useFormik({
-        initialValues: userData,
-        onSubmit: submitHandler,
-    });
+    const { handleSubmit, handleChange, values, errors, touched, handleBlur } =
+        useFormik({
+            initialValues: userData,
+            onSubmit: submitHandler,
+            validationSchema: signupSchema,
+        });
     return (
         <>
             <Container className="py-5">
@@ -26,48 +37,83 @@ function SignUp() {
                     <h2>Register Now :</h2>
                     <form
                         onSubmit={handleSubmit}
-                        className="d-flex align-items-start flex-column gap-2"
+                        className="d-flex  flex-column gap-2"
                     >
                         <label htmlFor="name">name:</label>
                         <input
                             value={values.name}
                             onChange={handleChange}
+                            onBlur={handleBlur}
                             id="name"
                             type="text"
                             className="form-control"
                         />
+                        {errors.name && touched.name && (
+                            <div className="alert-danger alert">
+                                {errors.name}
+                            </div>
+                        )}
+
                         <label htmlFor="email">email:</label>
                         <input
                             value={values.email}
                             onChange={handleChange}
+                            onBlur={handleBlur}
                             id="email"
                             type="text"
                             className="form-control"
                         />
+                        {errors.email && touched.email && (
+                            <div className="alert-danger alert">
+                                {errors.email}
+                            </div>
+                        )}
+
                         <label htmlFor="password">password:</label>
                         <input
                             value={values.password}
                             onChange={handleChange}
+                            onBlur={handleBlur}
                             id="password"
                             type="text"
                             className="form-control"
                         />
+                        {errors.password && touched.password && (
+                            <div className="alert-danger alert">
+                                {errors.password}
+                            </div>
+                        )}
+
                         <label htmlFor="rePassword">rePassword:</label>
                         <input
                             value={values.rePassword}
                             onChange={handleChange}
+                            onBlur={handleBlur}
                             id="rePassword"
                             type="text"
                             className="form-control"
                         />
+                        {errors.rePassword && touched.rePassword && (
+                            <div className="alert-danger alert">
+                                {errors.rePassword}
+                            </div>
+                        )}
+
                         <label htmlFor="phone">phone:</label>
                         <input
                             id="phone"
                             type="tel"
                             value={values.phone}
                             onChange={handleChange}
+                            onBlur={handleBlur}
                             className="form-control"
                         />
+                        {errors.phone && touched.phone && (
+                            <div className="alert-danger alert">
+                                {errors.phone}
+                            </div>
+                        )}
+
                         <button
                             type="submit"
                             className="btn align-self-end bg-main text-white"
