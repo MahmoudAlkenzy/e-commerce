99082562eae2600862d85a186723d14fcfa10fe4
MahmoudAlkenzyy <105129954+MahmoudAlkenzyy@users.noreply.github.com>
2024-02-23 05:12:47 +0200

    create payment page

---
+++ src/pages/payment/Payment.jsx
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { BaseUrl } from '../../services/baseUrl';
+import { useContext, useRef } from 'react';
+import { CartContext } from '../../context/CartContext';
+import { useToken } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+
+export default function Payment() {
+    const detaildRef = useRef();
+    const phonedRef = useRef();
+    const cityRef = useRef();
+    const navigae = useNavigate();
+    const { cartId, clearCart } = useContext(CartContext);
+    const { token } = useToken();
+
+    return (
+        <>
+            <div className="w-50 mx-auto d-flex flex-column gap-3 py-5">
+                <div>
+                    <label className="mb-1" htmlFor="details">
+                        details
+                    </label>
+                    <input
+                        ref={detaildRef}
+                        placeholder="Enter your addres"
+                        className="form-control"
+                        id="details"
+                        type="text"
+                    />
+                </div>
+                <div>
+                    <label className="mb-1" htmlFor="phone">
+                        phone
+                    </label>
+                    <input
+                        ref={phonedRef}
+                        placeholder="Enter your phone"
+                        className="form-control"
+                        id="phone"
+                        type="text"
+                    />
+                </div>
+                <div>
+                    <label className="mb-1" htmlFor="city">
+                        city
+                    </label>
+                    <input
+                        ref={cityRef}
+                        placeholder=" enter your address"
+                        className="form-control"
+                        id="city"
+                        type="text"
+                    />
+                </div>
+                <button className="btn btn-main">Place order</button>
+            </div>
+        </>
+    );
+}
