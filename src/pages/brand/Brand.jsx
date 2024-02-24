210c9af2ced496a9a8c0bf69df83bf2aaa5ac2d4
MahmoudAlkenzyy <105129954+MahmoudAlkenzyy@users.noreply.github.com>
2024-02-24 14:42:45 +0200

    add all brands page

---
+++ src/pages/brand/Brand.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Spinner } from '../../ui/spinners/Spinners';
+import useProducts from '../../hooks/useProducts';
+import { Row, Container } from 'react-bootstrap';
+import Product from '../../ui/product/Product';
+
+export default function Brand() {
+    const { brand } = useParams();
+    const { products, isLoading } = useProducts();
+
+    if (isLoading) return <Spinner />;
+    console.log(brand, products);
+    const proBrands = products.filter((pro) => pro.brand.slug === brand);
+
+    console.log('proBrands', proBrands);
+    return (
+        <>
+            <Container style={{ minHeight: '100vh' }}>
+                <Row className="h-100">
+                    {!proBrands.length ? (
+                        <div className=" d-flex justify-content-center align-items-center ">
+                            <p className="display-4"> This Brand is empty</p>
+                        </div>
+                    ) : (
+                        proBrands.map((product, idx) => {
+                            return <Product product={product} key={idx} />;
+                        })
+                    )}
+                </Row>
+            </Container>
+        </>
+    );
+}
